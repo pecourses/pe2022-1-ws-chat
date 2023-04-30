73de3081c210bd16e72fa59f9050acffc07bdd5b
Viktoriia <pecourses>
2023-04-30 19:18:10 +0300

    Add io client

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+// 1
+import { io } from 'socket.io-client';
 
 const axiosOptions = {
   baseURL: 'http://127.0.0.1:5000/api',
@@ -10,3 +12,9 @@ export const getMessages = limit => apiInstance.get(`/messages?limit=${limit}`);
 
 export const createMessage = newMessage =>
   apiInstance.post('/messages', newMessage);
+
+// -------
+const socket = io('ws://localhost:5000');
+
+// socket.on('подія',()=>{})  - прослуховувати події ws сервера
+// socket.emit('подія', payload) - генерувати події, які прослуховує сервер
